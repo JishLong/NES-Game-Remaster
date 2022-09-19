70a9b527e3d984534fadc73607dd2a2054de44bd
hgray02 <hgray576@gmail.com>
2022-09-18 20:40:27 -0400

    Layout of enemy interface and abstract class - Started work on the Skeleton enemy class - Also added a .gitignore

---
+++ Sprint0/Enemies/Skeleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Enemies
+{
+    public class Skeleton : Enemy
+    {
+        public Skeleton()
+        {
+            this.health = 2;    // arbitrary health value. I think they die in like a single hit?
+        }
+
+        public override void destroy()
+        {
+            // not sure what to do here yet...
+            throw new NotImplementedException();
+        }
+        public override void update()
+        {
+            
+        }
+
+        public override void draw()
+        {
+            // TODO: public properties for the graphics device manager and the sprite batch from the main game class?
+            //psuedo-code:
+            // sb = ZeldaGame.sprite_batch;
+            // graphics = ZeldaGame.graphics_device_manager;
+            // w = graphics.w;
+            // h = graphics.h;
+            // sprite.Draw(sb, w, h);
+        }
+
+    }
+}
