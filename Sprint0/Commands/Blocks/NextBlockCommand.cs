63b73f98de7b89c09ad95f35462a8d4d6016ad67
Hanebal <aseged.2@osu.edu>
2022-09-25 22:39:17 -0400

    Added block switching commands and keyboard input

---
+++ Sprint0/Commands/Blocks/NextBlockCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace Sprint0.Commands.Blocks
+{
+    public class NextBlockCommand : ICommand
+    {
+        private Game1 game;
+        public NextBlockCommand(Game1 game)
+        {
+            this.game = game;
+        }
+        public void Execute()
+        {
+            if (game.currentBlock < game.blocks.Length - 1)
+            {
+                game.currentBlock++;
+            }
+            else
+            {
+                game.currentBlock = 0;
+            }
+        }
+    }
+}
