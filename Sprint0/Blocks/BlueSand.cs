cd27a5902320de04c1982700ea6ca4410848946c
Hanebal <aseged.2@osu.edu>
2022-09-25 21:56:14 -0400

    Added all block classes

---
+++ Sprint0/Blocks/BlueSand.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Sprites.Blocks;
+
+namespace Sprint0.Blocks
+{
+    public class BlueSand : IBlock
+    {
+        // Sprite
+        private ISprite sprite;
+
+        // Coordinates and dimensions
+        private int x, y, w, h;
+
+        public BlueSand(int x, int y)
+        {
+            sprite = new BlueSandSprite();
+
+            this.x = x;
+            this.y = y;
+            w = 64;
+            h = 64;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sprite.Draw(sb, x, y, w, h);
+        }
+
+        public void Update()
+        {
+            // Nothing here?
+        }
+    }
+}
---
+++ Sprint0/Blocks/FireBlock.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Sprites.Blocks;
+
+namespace Sprint0.Blocks
+{
+    public class FireBlock : IBlock
+    {
+        // Sprite
+        private ISprite sprite;
+
+        // Coordinates and dimensions
+        private int x, y, w, h;
+
+        public FireBlock(int x, int y)
+        {
+            sprite = new FireBlockSprite();
+
+            this.x = x;
+            this.y = y;
+            w = 64;
+            h = 64;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sprite.Draw(sb, x, y, w, h);
+        }
+
+        public void Update()
+        {
+            // Nothing here?
+        }
+    }
+}
+
