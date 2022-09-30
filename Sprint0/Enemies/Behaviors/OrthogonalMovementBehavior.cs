21d7afefc851a6f2f5b7ee49b7e6c34a05e61918
hgray2 <hgray576@gmail.com>
2022-09-30 13:37:11 -0400

    Setting up behaviors

---
+++ Sprint0/Enemies/Behaviors/OrthogonalMovementBehavior.cs
@@ -0,0 +1,22 @@
+﻿using Sprint0.Enemies.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0.Enemies.Behaviors
+{
+    public class OrthogonalMovementBehavior : IMovementBehavior
+    {
+        public OrthogonalMovementBehavior()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+    }
+}
