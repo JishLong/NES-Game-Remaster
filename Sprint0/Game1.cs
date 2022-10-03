6c8547f817ea77ce0d884cbcee536ce92f0446ee
hgray2 <hgray576@gmail.com>
2022-10-03 13:38:05 -0400

    Okay, for real this time.

--- Sprint0/Game1.cs
+++ Sprint0/Game1.cs
@@ -5,17 +5,10 @@ using Sprint0.Items;
 using Sprint0.Player;
 using Sprint0.Bosses.Interfaces;
 using Sprint0.Bosses.Utils;
-using Sprint0.Npcs.Interfaces;
-using Sprint0.Npcs.Utils;
 using Sprint0.Sprites;
-using Sprint0.Enemies.Interfaces;
-using Sprint0.Enemies.Utils;
 using Sprint0.Blocks;
 using Sprint0.Blocks.Utils;
 using Sprint0.Items.Utils;
-using Sprint0.Bosses;
-using Sprint0.Enemies;
-using Sprint0.Npcs;
 using Sprint0.Characters;
 
 namespace Sprint0
@@ -35,23 +28,12 @@ namespace Sprint0
 
         // Boss stuff
         //public IBoss[] Bosses;
-        //public string[] BossTypes;
-        //public IBoss CurrentBoss { get; set; }
-        //public BossFactory BossFactory;
-        //public Vector2 BossPosition;
+        public string[] BossTypes;
+        public BossFactory BossFactory;
+        public Vector2 BossPosition;
         public IBoss CurrentBossProj1 { get; set; }
         public IBoss CurrentBossProj2 { get; set; }
         public IBoss CurrentBossProj3 { get; set; }
-
-        // NPC stuff
-        //public string[] NpcTypes;
-        //public INpc CurrentNpc { get; set; }
-        //public NpcFactory NpcFactory;
-        //public Vector2 NpcPosition;
-
-        //public ICharacter[] characters;
-        //public Vector2 CharacterPosition;
-        //public int currentCharacterIndex { get; set; }
 
         public Game1()
         {
@@ -69,15 +51,10 @@ namespace Sprint0
             // Item and block initialization
             CurrentItem = ItemFactory.GetInstance().GetNextItem(ItemFactory.DefaultItemPosition);
             CurrentBlock = BlockFactory.GetInstance().GetNextBlock(BlockFactory.DefaultBlockPosition);
-            CurrentCharacter = CharacterFactory.GetInstance().GetNextCharacter(CharacterFactory.DefaultCharacterPosition);
-            //currentCharacterIndex = 0;
-            //CharacterPosition = new Vector2(500, 200);
 
             // Boss and NPC stuff
-            //BossFactory = new BossFactory();
-            //NpcFactory = new NpcFactory();
-            //BossPosition = new Vector2(600, 200);
-            //NpcPosition = new Vector2(0, 200);
+            BossFactory = new BossFactory();
+            BossPosition = new Vector2(600, 200);
 
             Keyboard = new KeyboardController(this, Player.GetStateController());
 
@@ -89,45 +66,21 @@ namespace Sprint0
             SBatch = new SpriteBatch(GraphicsDevice);
             Resources.LoadContent(Content);
 
-            // Enemy currently instantiated here because of texture loading issue - soon to be fixed
-            CurrentCharacter = CharacterFactory.GetInstance().GetNextCharacter(CharacterFactory.DefaultCharacterPosition);
 
-            // Boss and NPC stuff
-           /* BossTypes = new string[]
+            BossTypes = new string[]
             {
                 "DODONGO",
                 "AQUAMENTUS",
                 "AQUAMENTUSFLAME",
             };
-            NpcTypes = new string[]
-            {
-                "OLDMAN",
-                "FLAME",
-                "BOMBPROJ",
-            };
-*/
- /*           characters = new ICharacter[] {
-                    new OldMan(CharacterPosition),
-                    new Flame(CharacterPosition),
-                    new BombProj(CharacterPosition),
-                    new Skeleton(CharacterPosition),
-                    new Bat(CharacterPosition),
-                    new Hand(CharacterPosition),
-                    new Gel(CharacterPosition),
-                    new RedGoriya(CharacterPosition),
-                    new Zol(CharacterPosition),
-                    new Dodongo(CharacterPosition),
-                    new Aquamentus(CharacterPosition)
-            };
-*/
 
- //           CurrentBoss = this.BossFactory.GetBoss(BossTypes[0], BossPosition);
-   //         CurrentNpc = this.NpcFactory.GetNpc(NpcTypes[1], NpcPosition);
+            // Character currently instantiated here because of texture loading issue - soon to be fixed
+            CurrentCharacter = CharacterFactory.GetInstance().GetNextCharacter(CharacterFactory.DefaultCharacterPosition);
 
             // For Aquamentus flames - need to refactor later
-            //CurrentBossProj1 = this.BossFactory.GetBoss(BossTypes[2], BossPosition);
-            //CurrentBossProj2 = this.BossFactory.GetBoss(BossTypes[2], BossPosition);
-            //CurrentBossProj3 = this.BossFactory.GetBoss(BossTypes[2], BossPosition);
+            CurrentBossProj1 = this.BossFactory.GetBoss(BossTypes[2], BossPosition);
+            CurrentBossProj2 = this.BossFactory.GetBoss(BossTypes[2], BossPosition);
+            CurrentBossProj3 = this.BossFactory.GetBoss(BossTypes[2], BossPosition);
         }
 
         protected override void Update(GameTime gameTime)
@@ -142,12 +95,11 @@ namespace Sprint0
             CurrentBlock.Update();
             CurrentCharacter.Update(gameTime);
 
-            // Boss and NPC stuff
-            //CurrentBoss.Update(gameTime);
-            //CurrentNpc.Update(gameTime);
-            //CurrentBossProj1.Update(gameTime);
-            //CurrentBossProj2.Update(gameTime);
-            //CurrentBossProj3.Update(gameTime);
+            // Boss projectiles.
+            CurrentBossProj1.Update(gameTime);
+            CurrentBossProj2.Update(gameTime);
+            CurrentBossProj3.Update(gameTime);
+
             Player.Update();
 
             base.Update(gameTime);
@@ -160,17 +112,15 @@ namespace Sprint0
 
             Player.Draw(SBatch, Graphics.PreferredBackBufferWidth, Graphics.PreferredBackBufferHeight);
 
-            // Draw the item, block, and enemy
+            // Draw the item, block, and character.
             CurrentItem.Draw(SBatch);
             CurrentBlock.Draw(SBatch);
             CurrentCharacter.Draw(SBatch);
 
-            // Boss and NPC stuff
-            //CurrentBossProj1.Draw(SBatch);
-            //CurrentBossProj2.Draw(SBatch);
-            //CurrentBossProj3.Draw(SBatch);
-            //CurrentBoss.Draw(SBatch);
-            //CurrentNpc.Draw(SBatch);
+            // Boss projectiles.
+            CurrentBossProj1.Draw(SBatch);
+            CurrentBossProj2.Draw(SBatch);
+            CurrentBossProj3.Draw(SBatch);
 
             SBatch.End();
 
