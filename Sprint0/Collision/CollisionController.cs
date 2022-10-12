73f2670b20775bea879fabe62c32c07683d79eed
JishLong <josh@alstongrove.com>
2022-10-11 23:22:18 -0400

    Added item-player collision handling

--- Sprint0/Collision/CollisionController.cs
+++ Sprint0/Collision/CollisionController.cs
@@ -2,6 +2,11 @@
 
 namespace Sprint0.Collision
 {
+    /* NOT IMPLEMENTED YET:
+     * 
+     * This class will basically be the way that clients utilized the collision system
+     * 
+     */
     public class CollisionController : IController
     {
         public void Update()
