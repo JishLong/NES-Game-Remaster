11a9b8224c3bd1bb8bba49b7c58e4a910f9681f5
Ethan Powers <ethandpowers@gmail.com>
2022-10-13 16:28:05 -0400

    player sword attack animation clipping fixed

--- Sprint0/Sprites/Player/Attack/SwordAttack/PlayerSwordAttackRight.cs
+++ Sprint0/Sprites/Player/Attack/SwordAttack/PlayerSwordAttackRight.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Player.State;
 
 namespace Sprint0.Sprites.Player.Attack.SwordAttack
 {
@@ -8,22 +9,38 @@ namespace Sprint0.Sprites.Player.Attack.SwordAttack
         // Singleton instance
         private static PlayerSwordAttackRight instance;
 
-        public static PlayerSwordAttackRight GetInstance()
+        public static PlayerSwordAttackRight GetInstance(PlayerStateController stateController)
         {
             if (instance == null)
             {
-                instance = new PlayerSwordAttackRight();
+                instance = new PlayerSwordAttackRight(stateController);
             }
             return instance;
         }
 
-        private PlayerSwordAttackRight() : base(4, 8)
+        private PlayerSwordAttackRight(PlayerStateController stateController) : base(4, 8)
         {
-            
+            this.stateController = stateController;
         }
+
+        private readonly PlayerStateController stateController;
 
         protected override Texture2D GetSpriteSheet() => Resources.LinkSpriteSheet;
 
         protected override Rectangle GetFirstFrame() => Resources.LinkSwordRight;
+
+        public override void Update()
+        {
+            Timer = (Timer + 1) % Speed;
+            if (Timer == 0)
+            {
+                CurrentFrame++;
+                if (CurrentFrame == NumFrames - 1)
+                {
+                    CurrentFrame = 0;
+                    stateController.GetState().StopAttacking();
+                }
+            }
+        }
     }
 }
