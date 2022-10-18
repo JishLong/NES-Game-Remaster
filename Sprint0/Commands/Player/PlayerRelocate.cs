f1c2bc7a60dd8b9e8c66f1eafde17b5bd41e8727
PaulFierman <110355809+PaulFierman@users.noreply.github.com>
2022-10-18 12:20:33 -0400

    Command to update players location

---
+++ Sprint0/Commands/Player/PlayerRelocate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint0.Player;
+
+namespace Sprint0.Commands.Player
+{
+    public class PlayerRelocate : ICommand
+    {
+        private readonly IPlayer Player;
+        private Vector2 NewLoc;
+
+        public PlayerRelocate(IPlayer player, Vector2 newLoc)
+        {
+            Player = player;
+            NewLoc = newLoc;
+        }
+
+        public void Execute()
+        {
+            Player.location(this.NewLoc);
+        }
+    }
+}
