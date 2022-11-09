beca9f00a4936d5407117e0ed4f7a3c69ac985ac
PaulFierman <110355809+PaulFierman@users.noreply.github.com>
2022-11-09 18:01:39 -0500

    Arrows = Rupees

--- Sprint0/Commands/Player/PlayerArrowAttackCommand.cs
+++ Sprint0/Commands/Player/PlayerArrowAttackCommand.cs
@@ -13,8 +13,14 @@ namespace Sprint0.Commands.Player
 
         public void Execute()
         {
-            Player.SecondaryWeapon = Types.Projectile.ARROW_PROJ;
-            new PlayerSecondaryAttackCommand(Player).Execute();
+            if (Player.Inventory.HasItem(Types.Item.RUPEE) && Player.Inventory.GetAmount(Types.Item.RUPEE) > 0)
+            {
+                Player.Inventory.decAmount(Types.Item.RUPEE);
+                Player.SecondaryWeapon = Types.Projectile.ARROW_PROJ;
+                new PlayerSecondaryAttackCommand(Player).Execute();
+            }
+
+            
         }
     }
 }
