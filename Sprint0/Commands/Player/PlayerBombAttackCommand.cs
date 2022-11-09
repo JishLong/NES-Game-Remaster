e86cd5fd0776e85b8fcde038ae1508a608284bd3
PaulFierman <110355809+PaulFierman@users.noreply.github.com>
2022-11-09 17:47:28 -0500

    Can only use bombs in the inventory

--- Sprint0/Commands/Player/PlayerBombAttackCommand.cs
+++ Sprint0/Commands/Player/PlayerBombAttackCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Sprint0.Player;
+using static Sprint0.Types;
 
 namespace Sprint0.Commands.Player
 {
@@ -13,8 +14,12 @@ namespace Sprint0.Commands.Player
 
         public void Execute()
         {
-            Player.SecondaryWeapon = Types.Projectile.BOMB_PROJ;
-            new PlayerSecondaryAttackCommand(Player).Execute();
+            if (Player.Inventory.HasItem(Types.Item.BOMB) && Player.Inventory.GetAmount(Types.Item.BOMB) > 0)
+            {
+                Player.Inventory.decAmount(Types.Item.BOMB);
+                Player.SecondaryWeapon = Types.Projectile.BOMB_PROJ;
+                new PlayerSecondaryAttackCommand(Player).Execute();
+            }
         }
     }
 }
